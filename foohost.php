3c34e0728edc83ac65ea69e9c314c7dd7f5d0dd6
Steve Teale <steve.teale@britseyeview.com>
2016-03-20 12:01:53 +0300

    Delete foohost.php

--- foohost.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-header("Content-type: text/plain");
-require 'foo.php';
-$a=array(1,1,3,4,5,6,7,8,9,10,11,12,13,14);
-echo writePost($a,3);
-?>
