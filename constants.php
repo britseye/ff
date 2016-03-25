2ebc466d9eda7c0036cf5c3cc23f6fc490f665ec
Steve Teale <steve.teale@britseyeview.com>
2016-03-25 07:38:15 +0300

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,6 +1,6 @@
 <?php
+// Facebook stuff
 $feedhost = "localhost";
-$appid = "450525878309993";
-$secret = "004f49ff7b257166a67fffb65a0fdd32";
-$anon = "/home/steve/bev/common/anon.png";
+$appid = "whatever";
+$secret = "whatever";
 ?>
